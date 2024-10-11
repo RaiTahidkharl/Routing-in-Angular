f937bbeb07a89792df821e17138459b20c1da4d3
Rai Tahid <raitahid@de-coders.net>
2024-10-11 13:35:36 +0500

    learning routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { DashboradComponent } from './dashborad/dashborad.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContentUSComponent } from './content-us/content-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component:DashboradComponent},
+    {path:'about-us',component:AboutUsComponent},
+    {path:'content-us',component:ContentUSComponent}
+];
---
+++ src/app/content-us/content-us.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-us',
+  standalone: true,
+  imports: [],
+  templateUrl: './content-us.component.html',
+  styleUrl: './content-us.component.css'
+})
+export class ContentUSComponent {
+
+}
